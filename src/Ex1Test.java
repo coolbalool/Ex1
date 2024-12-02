d319a77ddba79c09d602d536d1f5eebaac78a540
itama <itamarca117@gmail.com>
2024-12-02 14:31:15 +0200

    started adding tests

--- src/Ex1Test.java
+++ src/Ex1Test.java
@@ -1,6 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class Ex1Test
 {
 
+@Test
+    public void testCharToInt()
+{
+    for (int i = -9; i <= 9;i++)
+        if (i <= 9 && i >= 1)
+            assertEquals(i,Ex1.charToInt((char)i));
+    else assertEquals(-1,Ex1.charToInt((char)i));
+}
 
 
 }
