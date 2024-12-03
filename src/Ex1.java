d7152a00e207d472464cfc9cd52de0befec562a1
itama <itamarca117@gmail.com>
2024-12-03 13:14:01 +0200

    fixed almost all bugs and edge cases need to make sure it runs as the .jar and to add tests

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,132 +1,141 @@
-//package assignments.ex1;
-/// This class represents a simple solution for Ex1.
-/// As defined here: https://docs.google.com/document/d/1AJ9wtnL1qdEs4DAKqBlO1bXCM6r6GJ_J/r/edit/edit
-/// In this assignment, we will design a number formatting converter and calculator.
-/// In general, we will use Strings as numbers over basis of binary till Hexa.
-/// [2-16], 10-16 are represented by A,B,..G.
-/// The general representation of the numbers is as a String with the following format:
-/// <number>**<base> e.g., &ldquo;135bA&rdquo; (i.e., &ldquo;135&rdquo;, as 10 is the default base), &ldquo;100111b2&rdquo;, &ldquo;12345b6&rdquo;,&rdquo;012b5&rdquo;, &ldquo;123bG&rdquo;, &ldquo;EFbG&rdquo;.
-/// The following are NOT in the format (not a valid number):
-/// &ldquo;b2&rdquo;, &ldquo;0b1&rdquo;, &ldquo;123b&rdquo;, &ldquo;1234b11&rdquo;, &ldquo;3b3&rdquo;, &ldquo;-3b5&rdquo;, &ldquo;3 b4&rdquo;, &ldquo;GbG&rdquo;, "", null,
-/// You should implement the following static functions:
-public class Ex1 {
-    /**
-     * Convert the given number (num) to a decimal representation (as int).
-     * It the given number is not in a valid format returns -1.
-     * @param ch a String representing a number in basis [2,16]
-     */
+    //package assignments.ex1;
+    /// This class represents a simple solution for Ex1.
+    /// As defined here: https://docs.google.com/document/d/1AJ9wtnL1qdEs4DAKqBlO1bXCM6r6GJ_J/r/edit/edit
+    /// In this assignment, we will design a number formatting converter and calculator.
+    /// In general, we will use Strings as numbers over basis of binary till Hexa.
+    /// [2-16], 10-16 are represented by A,B,..G.
+    /// The general representation of the numbers is as a String with the following format:
+    /// <number>**<base> e.g., &ldquo;135bA&rdquo; (i.e., &ldquo;135&rdquo;, as 10 is the default base), &ldquo;100111b2&rdquo;, &ldquo;12345b6&rdquo;,&rdquo;012b5&rdquo;, &ldquo;123bG&rdquo;, &ldquo;EFbG&rdquo;.
+    /// The following are NOT in the format (not a valid number):
+    /// &ldquo;b2&rdquo;, &ldquo;0b1&rdquo;, &ldquo;123b&rdquo;, &ldquo;1234b11&rdquo;, &ldquo;3b3&rdquo;, &ldquo;-3b5&rdquo;, &ldquo;3 b4&rdquo;, &ldquo;GbG&rdquo;, "", null,
+    /// You should implement the following static functions:
+    public class Ex1 {
+        /**
+         * Convert the given number (num) to a decimal representation (as int).
+         * It the given number is not in a valid format returns -1.
+         * @param ch a String representing a number in basis [2,16]
+         */
 
-    public static int charToInt(char ch)
-    {
-        if (ch >= '0' && ch <= '9' || ch >= 'A' && ch <= 'F' ||ch >= 'a' && ch <= 'f')
-        if (ch == 'A' || ch == 'a') return 10;
-        else if (ch == 'B' || ch == 'b') return 11;
-        else if (ch == 'C' || ch == 'c') return 12;
-        else if (ch == 'D' || ch == 'd') return 13;
-        else if (ch == 'E' || ch == 'e') return 14;
-        else if (ch == 'F' || ch == 'f') return 15;
-        else return ch - '0';
-        else return -1;
-    }
-
-    public static char intToChar(int num)
-    {
-        if (num < 0 || num > 15) return '-';
-        if (num == 10) return 'A';
-            else if (num == 11) return 'B';
-            else if (num == 12) return 'C';
-            else if (num == 13) return 'D';
-            else if (num == 14) return 'E';
-            else if (num == 15) return 'F';
-            else return (char) ('0' + num);
-    }
-
-    public static int number2Int(String num) {
-        if (!isNumber(num)) return -1;
-        int  base = charToInt(num.charAt(num.length()-1));
-        if (num.charAt(num.length()-1) == 'g' || num.charAt(num.length()-1) == 'G') base = 16;
-        int ans = 0;
-        int mult = 1;
-
-        for (int i = num.length() - 3;i >= 0;i--) // runs until the 2 last characters (b and the base)
+        public static int charToInt(char ch)
         {
-            ans += charToInt(num.charAt(i)) * mult;
-            mult *= base;
-        }
-        return ans;
-    }
-    /**
-     * This static function checks if the given String (g) is in a valid "number" format.
-     * @param a a String representing a number
-     * @return true iff the given String is in a number format
-     */
-    public static boolean isNumber(String a) {
-        if (a == null || a.length() < 3) return false;
-        boolean ans = true;
-        if(a.charAt(a.length()-2) != 'b') return false;
-        for (int i = 0; i < a.length(); i++ ) {
-            int num = charToInt(a.charAt(i));
-            if (num == -1 || num >= charToInt(a.charAt(a.length()-1))) return false;        }
 
-        return ans;
-    }
+            if ((ch >= '0' && ch <= '9')) return ch - '0';
+            else if (ch == 'A' || ch == 'a') return 10;
+            else if (ch == 'B' || ch == 'b') return 11;
+            else if (ch == 'C' || ch == 'c') return 12;
+            else if (ch == 'D' || ch == 'd') return 13;
+            else if (ch == 'E' || ch == 'e') return 14;
+            else if (ch == 'F' || ch == 'f') return 15;
+            return -1;
 
-    /**
-     * Calculate the number representation (in basis base)
-     * of the given natural number (represented as an integer).
-     * If num<0 or base is not in [2,16] the function should return "" (the empty String).
-     * @param num the natural number (include 0).
-     * @param base the basis [2,16]
-     * @return a String representing a number (in base) equals to num, or an empty String (in case of wrong input).
-     */
-    public static String int2Number(int num, int base) {
-        StringBuilder ans = new StringBuilder();
-        int division = num;
-
-        while (division >= 1)
-        {
-            ans.append(intToChar(division % base));
-            division -= division % base;
-            division /= base;
         }
 
-        return new StringBuilder(ans.toString()).reverse().toString(); // returns the reversed array
-    }
-
-    /**
-     * Checks if the two numbers have the same value.
-     * @param n1 first number
-     * @param n2 second number
-     * @return true iff the two numbers have the same values.
-     */
-    public static boolean equals(String n1, String n2) {
-        if (isNumber(n1) && isNumber(n2))
-            return (number2Int(n1) == number2Int(n2));
-        return false;
-    }
-
-    /**
-     * This static function search for the array index with the largest number (in value).
-     * In case there are more than one maximum - returns the first index.
-     * Note: you can assume that the array is not null and is not empty, yet it may contain null or none-valid numbers (with value -1).
-     * @param arr an array of numbers
-     * @return the index in the array in with the largest number (in value).
-     *
-     */
-    public static int maxIndex(String[] arr) {
-        int ans = 0;
-        int maxValue = 0;
-
-        for (int i = 0; i < arr.length; i++)
+        public static char intToChar(int num)
         {
-            int numberValue = number2Int(arr[i]);
-            if (numberValue > maxValue)
+            if (num < 0 || num > 15) return '-';
+            if (num == 10) return 'A';
+                else if (num == 11) return 'B';
+                else if (num == 12) return 'C';
+                else if (num == 13) return 'D';
+                else if (num == 14) return 'E';
+                else if (num == 15) return 'F';
+                else return (char) ('0' + num);
+        }
+
+        public static int number2Int(String num) {
+            if (!isNumber(num)) return -1;
+            int  base = charToInt(num.charAt(num.length()-1));
+            if (num.charAt(num.length()-1) == 'G') base = 16;
+            int ans = 0;
+            int mult = 1;
+
+            for (int i = num.length() - 3;i >= 0;i--) // runs until the 2 last characters (b and the base)
             {
-            maxValue = numberValue;
-            ans = i;
+                ans += charToInt(num.charAt(i)) * mult;
+                mult *= base;
             }
+            return ans;
+        }
+        /**
+         * This static function checks if the given String (g) is in a valid "number" format.
+         * @param a a String representing a number
+         * @return true iff the given String is in a number format
+         */
+        public static boolean isNumber(String a) {
+            if (a == null || a.length() < 3) return false;
+
+            int base = charToInt(a.charAt(a.length()-1));
+            if (a.charAt(a.length()-1) == 'G') base = 16;
+            if (base == -1) return false;
+
+            if(a.charAt(a.length()-2) != 'b') return false;
+
+            boolean ans = true;
+            for (int i = 0; i < a.length() -2; i++ ) {
+                int num = charToInt(a.charAt(i));
+                if (num == -1 || num >= base) return false;        }
+
+            return ans;
         }
 
-        return ans;
+        /**
+         * Calculate the number representation (in basis base)
+         * of the given natural number (represented as an integer).
+         * If num<0 or base is not in [2,16] the function should return "" (the empty String).
+         * @param num the natural number (include 0).
+         * @param base the basis [2,16]
+         * @return a String representing a number (in base) equals to num, or an empty String (in case of wrong input).
+         */
+        public static String int2Number(int num, int base) {
+            StringBuilder ans = new StringBuilder();
+            int division = num;
+            if (num == 0) return "0b" + intToChar(base);
+
+            while (division >= 1)
+            {
+                ans.append(intToChar(division % base));
+                division -= division % base;
+                division /= base;
+            }
+            if (num < 0 || base < 2 || base > 16) return "";
+            ans.reverse();
+            return ans. append("b").append(intToChar(base)).toString();
+        }
+
+        /**
+         * Checks if the two numbers have the same value.
+         * @param n1 first number
+         * @param n2 second number
+         * @return true iff the two numbers have the same values.
+         */
+        public static boolean equals(String n1, String n2) {
+            if (isNumber(n1) && isNumber(n2))
+                return (number2Int(n1) == number2Int(n2));
+            return false;
+        }
+
+        /**
+         * This static function search for the array index with the largest number (in value).
+         * In case there are more than one maximum - returns the first index.
+         * Note: you can assume that the array is not null and is not empty, yet it may contain null or none-valid numbers (with value -1).
+         * @param arr an array of numbers
+         * @return the index in the array in with the largest number (in value).
+         *
+         */
+        public static int maxIndex(String[] arr) {
+            int ans = -1;
+            int maxValue = -1;
+
+            for (int i = 0; i < arr.length; i++)
+            {
+                int numberValue = number2Int(arr[i]);
+                if (numberValue > maxValue)
+                {
+                maxValue = numberValue;
+                ans = i;
+                }
+            }
+
+            return ans;
+        }
     }
-}
--- src/Ex1Main.java
+++ src/Ex1Main.java
@@ -4,33 +4,46 @@ import java.util.Scanner;
 public class Ex1Main {
     public static void main(String[] args)
     {
-
         Scanner in = new Scanner(System.in);
         String[] nums = new String[4];
-        // first number
-        System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
-        String num1 = in.nextLine();
-        if (num1.equals("quit")) return;
-        nums[0] = num1;
-        int valNum1 = Ex1.number2Int(num1);
-        System.out.println("num1= "+num1+" is number: "+Ex1.isNumber(num1)+" , value: "+valNum1);
-// second number
-        System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
-        String num2 = in.nextLine();
-        nums[1] = num2;
-        int valNum2 = Ex1.number2Int(num2);
-        if (num2.equals("quit")) return;
-        System.out.println("num2= "+num2+" is number: "+Ex1.isNumber(num2)+" , value: "+valNum2);
+
+        nums[0] = assignNumber(1);
+        if (nums[0].equals("Err")) return;
+        int val1 = Ex1.number2Int(nums[0]);
+
+        nums[1] = assignNumber(2);
+        if (nums[1].equals("Err")) return;
+        int val2 = Ex1.number2Int(nums[1]);
+
         // result
         System.out.println("Enter a base for output: (a number [2,16] ");
-        int base = in.nextInt();
-        String sumNum = Ex1.int2Number(valNum1 + valNum2,base);
-        String multNum = Ex1.int2Number(valNum1 + valNum2,base);
+        String baseStr = in.nextLine();
+        int base;
+        if (baseStr.length() >1) base = Integer.parseInt(baseStr);
+        else base = Ex1.charToInt(baseStr.charAt(0));
+
+        String sumNum = Ex1.int2Number(val1 + val2,base);
+        String multNum = Ex1.int2Number(val1 * val2,base);
         nums[2] = sumNum;
         nums[3] = multNum;
-        System.out.println(num1 + " + " + num2 +" = " +sumNum);
-        System.out.println(num1 + " * " + num2 +" = " +multNum);
+        System.out.println(nums[0] + " + " + nums[1] +" = " +sumNum);
+        System.out.println(nums[0] + " * " + nums[1] +" = " +multNum);
         System.out.println("Max number over "+Arrays.toString(nums)+" is: " + nums[Ex1.maxIndex(nums)]);
 
     }
+
+    public static String assignNumber(int index)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a string as number#"+index+" (or \"quit\" to end the program): ");
+        String num = in.nextLine();
+        if (num.equals("quit")) {
+            System.out.println("Quiting...");
+            return "Err";}
+        int valNum1 = Ex1.number2Int(num);
+        System.out.println("num"+index+"= "+num+" is number: "+Ex1.isNumber(num)+" , value: "+valNum1);
+        return num;
+    }
+
+
 }
